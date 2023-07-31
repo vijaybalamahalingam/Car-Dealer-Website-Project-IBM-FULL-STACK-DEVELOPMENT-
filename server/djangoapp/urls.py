87fd01bf516a6713f8f61ab18290690ecb742941
Vijay M <136885873+vijaybalamahalingam@users.noreply.github.com>
2023-07-31 19:31:30 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -25,6 +25,21 @@ urlpatterns = [
 
     # path for dealer reviews view
 
+# route for get_dealerships
+
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+    path(route='', view=views.get_dealerships, name='index')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
